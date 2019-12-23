aa9a658d4d6322ab04d0848ce9775aa739f786cf
Sean Hellum <seanhellum45@gmail.com>
2019-12-23 23:13:05 +0000

    add comments to fib.

--- fib.hs
+++ fib.hs
@@ -1,7 +1,7 @@
-fib :: Int -> Int
-fib 0 = 0
-fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
+fib :: Int -> Int -- define funtion as taking one integer and returning one integer
+fib 0 = 0 -- return 0 if input is 0
+fib 1 = 1 -- reutrn 1 if input is 1
+fib n = fib (n-1) + fib (n-2) -- else compute fibonacci and return it
 
 main = do
-    putStrLn (show(fib 6))
+    putStrLn (show(fib 6)) -- compute the 6th fibonacci number
