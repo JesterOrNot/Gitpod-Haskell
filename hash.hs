b7e57bd287835d2479b57b199dc2f3185b797359
Sean Hellum <seanhellum45@gmail.com>
2019-12-24 02:56:26 +0000

    update more hash,hs to reflect conditionals

--- hash.hs
+++ hash.hs
@@ -2,4 +2,6 @@ import Data.HashMap.Lazy
 
 main = do
     let hashMap = fromList [(1 :: Int, 'a'), (2, 'b'), (3, 'c')]
-    putStrLn (show ('c' `elem` (elems hashMap)))
+    if 'c' `elem` hashMap
+        then putStrLn "c is the keys"
+        else putStrLn "c is NOT in the keys"
