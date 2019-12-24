9f903948a8bdeb9fa58203ec2639676be29e733e
Sean Hellum <seanhellum45@gmail.com>
2019-12-24 03:53:07 +0000

    reflect 'c' locale in output for `hash.hs`

--- hash.hs
+++ hash.hs
@@ -3,5 +3,5 @@ import Data.HashMap.Lazy
 main = do
     let hashMap = fromList [(1 :: Int, 'a'), (2, 'b'), (3, 'c')]
     if 'c' `elem` hashMap
-        then putStrLn "c is the keys"
-        else putStrLn "c is NOT in the keys"
+        then putStrLn "c is the values"
+        else putStrLn "c is NOT in the values"
