041865470980cb09fcc52cab2e845a0502358c09
Sean Hellum <seanhellum45@gmail.com>
2019-12-24 02:50:02 +0000

    refactor hash.hs

--- hash.hs
+++ hash.hs
@@ -1,8 +1,5 @@
 import Data.HashMap.Lazy
-isMember n [] = False
-isMember n (x:xs)
-    | n == x = True
-    | otherwise = isMember n xs
+
 main = do
     let hashMap = fromList [(1 :: Int, 'a'), (2, 'b'), (3, 'c')]
-    putStrLn (show (isMember 'e' (elems hashMap)))
+    putStrLn (show ('c' `elem` (elems hashMap)))
