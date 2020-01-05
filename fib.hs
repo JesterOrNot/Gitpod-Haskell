2cab31684382f7fe5ae3c85d41ab372d7c559e73
Sean Hellum <seanhellum45@gmail.com>
2020-01-05 01:01:49 +0000

    update names

--- fib.hs
+++ fib.hs
@@ -1,10 +1,10 @@
-fib :: Int -> Integer
+fibonacci :: Int -> Integer
 main :: IO ()
 
 main = do
-    putStrLn(show(fib 10000))
+    putStrLn(show(fibonacci 10000))
 
-fib = (map fibs [0 ..] !!)
-   where fibs 0 = 0
-         fibs 1 = 1
-         fibs n = fib (n-2) + fib (n-1)
+fibonacci = (map fib [0 ..] !!)
+   where fib 0 = 0
+         fib 1 = 1
+         fib n = fibonacci (n-2) + fibonacci (n-1)
