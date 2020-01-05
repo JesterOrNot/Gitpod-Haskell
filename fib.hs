c019f6c81d19fe707013e8e28eb8d440630d1e0a
Sean Hellum <seanhellum45@gmail.com>
2020-01-05 00:37:44 +0000

    refactor

--- fib.hs
+++ fib.hs
@@ -1,10 +1,10 @@
 fib :: Int -> Integer
 main :: IO ()
 
+main = do
+    putStrLn(show(fib 10000))
+
 fib = (map fibs [0 ..] !!)
    where fibs 0 = 0
          fibs 1 = 1
          fibs n = fib (n-2) + fib (n-1)
-
-main = do
-    putStrLn(show(fib 10000))
