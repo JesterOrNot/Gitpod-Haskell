58e1053e4d912ecf303e6ae1fb0c09cd433ff1e5
Sean Hellum <seanhellum45@gmail.com>
2020-01-05 01:05:57 +0000

    update

--- fact.hs
+++ fact.hs
@@ -2,7 +2,7 @@ main :: IO ()
 factorial :: Int -> Integer
 
 main = do
-    putStrLn(show(factorial 500))
+        putStrLn(show(factorial 500))
 
 factorial = (map fact [0 ..] !!)
    where fact 0 = 1
