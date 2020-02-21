a30dc40230ff7c5bd7dc3eaf779a1ffa4e1e6dda
Sean Hellum <seanhellum45@gmail.com>
2020-02-21 18:14:24 +0000

    fix

--- cli.hs
+++ cli.hs
@@ -1,7 +1,9 @@
 import System.Environment   
 
+main :: IO ()
+
 main = do
     args <- getArgs
-    case args !! 0 of
+    case head args of
         "help" -> putStrLn "This is a help menu"
         _ -> putStrLn "Invalid"
